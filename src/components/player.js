343996f812f383d9b06f4308b8fdf6532558f8e4
Sam Smith <sjs5pg@virginia.edu>
2021-10-25 19:20:37 -0400

    kinda got metronome input working

--- src/components/player.js
+++ src/components/player.js
@@ -1,25 +1,49 @@
 import React from "react";
 
 const Player = () => {
-  let beatsPerSecond = (60 / 122) * 1000;
+  const [isPlaying, setIsPlaying] = React.useState(false);
+  const [metronomeInterval, setMetronomeInterval] = React.useState(null);
+  const [beatsPerSecond, setBeatsPerSecond] = React.useState(0);
 
   let myAudio = new Audio("/metronomeHit.mp3");
   const start = () => {
     myAudio.play();
   };
-  //   countFunc(beatsPerSecond);
   const countFunc = (beatsPerSecond) => {
     const myTimer = setInterval(() => {
       start();
     }, beatsPerSecond);
-
-    setTimeout(() => {
-      clearInterval(myTimer);
-    }, 6000);
+    setMetronomeInterval(myTimer);
   };
   return (
     <div>
-      <button onClick={() => countFunc(beatsPerSecond)}>Play Sound</button>
+      <button
+        onClick={() => {
+          setIsPlaying(!isPlaying);
+          let theInput = document.getElementById("bpmInput").value;
+          let numInput = parseInt(theInput);
+          numInput = (60 / numInput) * 1000;
+          console.log(numInput);
+          countFunc(numInput);
+        }}
+      >
+        Play
+      </button>
+
+      <button
+        onClick={() => {
+          clearInterval(metronomeInterval);
+        }}
+      >
+        Stop
+      </button>
+      <br />
+      <br />
+      <input
+        id="bpmInput"
+        type="text"
+        placeholder="Enter your desired BPM..."
+      />
     </div>
   );
 };
---
+++ src/helpers/calls.js
@@ -0,0 +1,20 @@
+const moves = [
+  "Sombrero",
+  "Enchufe Nueva York",
+  "Enchufe doble",
+  "Enchufe",
+  "Dame dos con dos ",
+  "Dame dos",
+  "Dame",
+  "Ping pong",
+  "Una bulla larga",
+  "Una bulla",
+  "Matala",
+];
+
+export default moves;
+
+// Un fly = called on 5
+// Fly doble = called on 5
+// Fly triple = called on 5
+// Fly higher = called on 5
